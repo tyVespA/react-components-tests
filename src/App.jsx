b61e401515c8c3683a4763097d760d262e585daa
tyVespA <migliavacca.marco08@gmail.com>
2023-11-18 00:20:52 +0100

    Add and test fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <>test</>;
+  return (
+    <main>
+      <p>The quick brown fox jumps over the lazy dog</p>
+      <h1>The quick brown fox jumps over the lazy dog</h1>
+      <em>The quick brown fox jumps over the lazy dog</em>
+      <br />
+      <i>The quick brown fox jumps over the lazy dog</i>
+    </main>
+  );
 }
 
 export default App;
