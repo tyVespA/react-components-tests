5938538e8f7c4b2f09412232e1824f808f854951
tyVespA <migliavacca.marco08@gmail.com>
2023-11-18 01:17:35 +0100

    Add MainCard(s), add stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import "./app.css";
+import MainCard from "./components/MainCard";
+
 function App() {
   return (
     <main>
@@ -6,6 +9,13 @@ function App() {
       <em>The quick brown fox jumps over the lazy dog</em>
       <br />
       <i>The quick brown fox jumps over the lazy dog</i>
+      <div className="cardsContainer">
+        <MainCard title="test1">test</MainCard>
+        <MainCard title="test1">test</MainCard>
+        <MainCard title="test1">test</MainCard>
+        <MainCard title="test1">test</MainCard>
+        <MainCard title="test1">test</MainCard>
+      </div>
     </main>
   );
 }
